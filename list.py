4619f7ae4cce94442981acad424814f3b7b034c1
Zippo001 <59906391+Zippo001@users.noreply.github.com>
2020-10-17 21:02:31 +0800

    Update list.py

--- list.py
+++ list.py
@@ -6,9 +6,13 @@
 
 a=[[0,1,2]*3]
 b=[[0,1,2] for i in range(3)]
+a2=[[0,1,2]]*3
 c=[1 *3]
 d=[1 for i in range(3)]
+c2=[1]*3
 print('a:',a)
 print('b:',b)
+print('a2:',a2)
 print('c:',c)
 print('d:',d)
+print('c2:',c2)
