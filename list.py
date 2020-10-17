5939183b8433d6627ae6d7d3db94e1de5fceadff
Zippo001 <59906391+Zippo001@users.noreply.github.com>
2020-10-17 09:38:24 +0800

    Add files via upload

---
+++ list.py
@@ -0,0 +1,14 @@
+# -*-   coding:utf-8    -*-
+'''
+作者（author）：张佳旺（Jiawang Zhang）
+日期（date）：2020-10-17
+'''
+
+a=[[0,1,2]*3]
+b=[[0,1,2] for i in range(3)]
+c=[1 *3]
+d=[1 for i in range(3)]
+print('a:',a)
+print('b:',b)
+print('c:',c)
+print('d:',d)
